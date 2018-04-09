05c5dd731ffc09d2c6520e62d763f6a87cbdbd6f
Thomas Szadek <37246650+thomisz@users.noreply.github.com>
2018-04-09 22:17:13 +0200

    Add files via upload

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,19 @@
-#write your code here
+def echo(reponse)
+return reponse
+end
+
+echo("hello")
+
+def shout(reponse)
+return reponse.upcase
+end
+
+shout("hello")
+
+def repeat(reponse, n = 2)
+return (reponse + " ")*(n-1) + reponse
+end
+
+def first_letter(reponse)
+return "hello".delete "ello"
+end
