55ed4dbfd789a18c759a59b70fd79630b0dc99b0
Thomas Szadek <37246650+thomisz@users.noreply.github.com>
2018-04-09 22:16:38 +0200

    Add files via upload

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,19 @@
-#write your code here
+def add(num,num1)
+  return num + num1
+end
+puts add(0,0)
+
+def subtract(num2,num3)
+   return num2 - num3
+end
+puts subtract(10,4)
+
+def sum(array)
+somme = 0
+array.each do |chiffre|
+   somme = somme + chiffre
+end
+return somme
+end
+
+sum ([1,3,5,7,9])
